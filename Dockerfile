1eb0e1cdda085f719108755b3730ff6a9d5ee1ac
kevin <kjwilde@hotmail.com>
2019-01-20 16:45:44 -0600

    Attempting to fix bad cert issue that is preventing lnd node from communicating with btcd.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,21 @@
-FROM golang:1.11.4-alpine as builder
+FROM golang:1.10-alpine as builder
 
 MAINTAINER Olaoluwa Osuntokun <lightning.engineering>
+
+# Copy in the local repository to build from.
+COPY . /go/src/github.com/lightningnetwork/lnd
 
 # Force Go to use the cgo based DNS resolver. This is required to ensure DNS
 # queries required to connect to linked containers succeed.
 ENV GODEBUG netdns=cgo
+
+## Install dependencies and install/build lnd.
+#RUN apk add --no-cache \
+#    git \
+#    make \
+#&&  cd /go/src/github.com/lightningnetwork/lnd \
+#&&  make \
+#&&  make install
 
 # Install dependencies and install/build lnd.
 RUN apk add --no-cache \
@@ -36,13 +47,11 @@ COPY --from=builder /go/bin/lnd /bin/
 RUN apk add --no-cache \
     bash
 
-#    \
-#    git \
-#&&  git clone https://github.com/lightningnetwork/lnd.git /go/src/github.com/lightningnetwork/lnd
-
 # Copy the entrypoint script.
 #COPY "docker/lnd/start-lnd.sh" .
-#COPY "start-lnd.sh" .
+#RUN chmod +x start-lnd.sh
+#
+#ENTRYPOINT ["/start-lnd.sh"]
 
 
 #COPY "docker/lnd.conf" /root/.lnd/
--- docker/start-lnd.sh
+++ docker/start-lnd.sh
@@ -3,8 +3,6 @@
 # exit from script if error was raised.
 set -e
 
-scriptArgs="$@"
-
 # error function is used within a bash function in order to send the error
 # message directly to the stderr output and exit.
 error() {
@@ -43,8 +41,7 @@ set_default() {
 # Set default variables if needed.
 RPCUSER=$(set_default "$RPCUSER" "devuser")
 RPCPASS=$(set_default "$RPCPASS" "devpass")
-#DEBUG=$(set_default "$DEBUG" "debug")
-DEBUG=$(set_default "$DEBUG" "trace")
+DEBUG=$(set_default "$DEBUG" "debug")
 NETWORK=$(set_default "$NETWORK" "simnet")
 CHAIN=$(set_default "$CHAIN" "bitcoin")
 BACKEND="btcd"
@@ -52,146 +49,22 @@ if [[ "$CHAIN" == "litecoin" ]]; then
     BACKEND="ltcd"
 fi
 
-DEPLOYMENT_NAME_DIR=""
-if [[ ! -z "$DEPLOYMENT_NAME" ]]; then
-    DEPLOYMENT_NAME_DIR="/$DEPLOYMENT_NAME"
-fi
-
-kill_lnd() {
-    pkill lnd || true
-}
-
-hostIp=`hostname -i`
 btcdHostIp=`cat /mnt/lk/shared/rpc/btcd-host-ip`
 echo "btcdHostIp: ${btcdHostIp}"
-#hostIp="test"
 
-start_lnd_cmd_test=" \
-        sleep 1 \
-    "
-
-background="&"
-
-start_lnd_cmd=" \
-    lnd \
-        --noseedbackup \
-        --$CHAIN.active \
-        --$CHAIN.$NETWORK \
-        --$CHAIN.node=\"btcd\" \
-        --$BACKEND.rpccert=\"/mnt/lk/shared/rpc/rpc.cert\" \
-        --$BACKEND.rpchost=\"$btcdHostIp\" \
-        --$BACKEND.rpcuser=\"$RPCUSER\" \
-        --$BACKEND.rpcpass=\"$RPCPASS\" \
-        --debuglevel=\"$DEBUG\" \
-        $scriptArgs \
-"
-
-#        --tlsextraip=* \
-#        --tlsextradomain=* \
-#        --restlisten=0.0.0.0:9735 \
-#        --rpclisten=0.0.0.0:10009 \
-#        --externalip=$hostIp \
-#        --logdir=\"/mnt/lk/shared/data\" \
-#        --$BACKEND.dir=\"/mnt/lk/shared/data\" \
-#--no-macaroons \
-#        --datadir=\"/mnt/lk/shared/data\" \
-#        --logdir=\"/mnt/lk/shared/data\" \
-#        --tlscertpath=/root/.lnd/tls.cert \
-#        --tlskeypath=/root/.lnd/tls.key \
+exec lnd \
+    --noseedbackup \
+    --logdir="/data" \
+    "--$CHAIN.active" \
+    "--$CHAIN.$NETWORK" \
+    "--$CHAIN.node"="btcd" \
+    "--$BACKEND.rpccert"="/mnt/lk/shared/rpc/rpc.cert" \
+    "--$BACKEND.rpchost"="$btcdHostIp" \
+    "--$BACKEND.rpcuser"="$RPCUSER" \
+    "--$BACKEND.rpcpass"="$RPCPASS" \
+    --debuglevel="$DEBUG" \
+    "$@"
 
 
-start_lnd() {
-
-    echo "start_lnd debug1"
-
-    exec lnd
-        --noseedbackup \
-        --logdir="/data" \
-        "--$CHAIN.active" \
-        "--$CHAIN.$NETWORK" \
-        "--$CHAIN.node"="btcd" \
-        "--$BACKEND.rpccert"="/mnt/lk/shared/rpc/rpc.cert" \
-        "--$BACKEND.rpchost"="lightning-kube-btcd.lightning-kube" \
-        "--$BACKEND.rpcuser"="$RPCUSER" \
-        "--$BACKEND.rpcpass"="$RPCPASS" \
-        --rpclisten=`hostname -i`:10009 \
-        --tlsextraip=`hostname -i` \
-        --debuglevel="$DEBUG" \
-        "$scriptArgs"
-
-    echo "start_lnd debug2"
-
-
-#        --rpclisten=$hostIp:10009 \
-#        --tlsextraip=$hostIp \
-
-#    "$@"
-#
-#    echo "start_lnd debug3"
-}
-
-#start_lnd_bg_kill() {
-#    start_lnd &
-#    kill_lnd
-#}
-
-lnd --version
-
-echo "debug1"
-
-echo ${start_lnd_cmd}
-
-echo "debug1.1"
-
-#`${start_lnd_cmd}` &
-#eval ${start_lnd_cmd} ${background} || true
-
-echo "debug2"
-
-#while [[ ! -f /root/.lnd/tls.cert || ! -f /root/.lnd/tls.cert ]]
-#do
-#    echo "waiting for tls files to be created..."
-#    sleep 2
-#done
-#
-#echo "debug3"
-
-#kill_lnd
-
-#lncli --network=simnet --no-macaroons stop || true
-
-#echo "debug4"
-#ls -al /root/.lnd
-#echo "debug4.1"
-#rm /root/.lnd/tls.cert
-#rm /root/.lnd/tls.key
-#echo "debug4.2"
-#ls -al /root/.lnd
-#
-#echo "debug5"
-#ls -al /data
-#echo "debug5.1"
-#rm -rf /data/*
-#echo "debug5.2"
-#ls -al /data
-
-
-
-echo "debug6"
-
-exec ${start_lnd_cmd}
-
-echo "debug7"
-
-#cp /mnt/lk/shared/rpc/rpc.cert /root/.lnd/tls.cert
-#sleep 10000
-
-
-
-#    --tlsextraip=0.0.0.0 \
-#--rpclisten=localhost:10009 \
-
-#    --datadir="/mnt/lk/lnd$DEPLOYMENT_NAME_DIR/data" \
-#    --logdir="/mnt/lk/lnd$DEPLOYMENT_NAME_DIR/log" \
-
-#    --macaroonpath="/mnt/lk/lnd$DEPLOYMENT_NAME_DIR/data/chain/bitcoin/simnet/admin.macaroon" \
+#    "--$BACKEND.rpccert"="/shared/rpc/rpc.cert" \
+#    "--$BACKEND.rpchost"="blockchain" \
